181d165f43e62dde4727b00a611f132d69167ca8
Matthew Smith <matthew.smith2@extendhealth.com>
2012-03-13 06:55:25 -0600

    Updated the npm header to define browser variables that are not set by jsdom.

--- node-npm-header.js
+++ node-npm-header.js
@@ -1,4 +1,6 @@
 var jsdom = require('jsdom').jsdom,
     document = jsdom('<html><head></head><body></body></html>'),
     window = document.createWindow(),
-    navigator = window.navigator;
+    navigator = window.navigator = {},
+    DEBUG = false;
+navigator.userAgent = 'NodeJs JsDom';
